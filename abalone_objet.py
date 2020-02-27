91c503bab662aa8836f652a290ae6df79d132491
mike <mike@alezan.org>
2020-02-27 22:39:26 +0100

    sumito et joueur courant

--- abalone_objet.py
+++ abalone_objet.py
@@ -27,10 +27,19 @@ class Board():
         self.last=[]
         self.select=[]
         self.sens=""
+        self.gain=[0,0,0]
+        c=plt.Circle((9.5,0.5),.2,color='red')
+        ax.add_patch(c)
         
     def swap(self):
         self.select=[]
         self.player,self.other=self.other,self.player
+        if self.player==1:
+            col='red'
+        else:
+            col='blue'
+        c=plt.Circle((9.5,0.5),.2,color=col)
+        ax.add_patch(c)
         
     def case(self,event):
         if event.xdata!=None and event.ydata!=None:
@@ -70,7 +79,8 @@ class Board():
             return
             
     def move(self,event):
-        dep=self.case(event)
+        dep=np.array(self.case(event))
+        select=np.array(self.select)
         print(dep)
         if self.sens=='ligne' or self.sens=='diag':
             ind_var=1
@@ -79,26 +89,81 @@ class Board():
             ind_var=0
             ind_fix=1
         if dep[2] in [0,self.other]:
+            point=None
+            dir=[(0,-1),(-1,0),(-1,-1),(0,1),(1,0),(1,1)]
+            if tuple(dep[:2]-select[0]) in dir:
+                point=0
+            elif tuple(dep[:2]-select[1]) in dir:
+                point=1
+            else:
+                self.state="Select"
+                affiche(self.M)
+                return 
             if self.sens=='point' and dep[2]==0:
                 self.M[self.select[0]]=0
                 self.M[dep[0],dep[1]]=self.player
                 self.swap()
-            elif self.sens in ['ligne','colonne','diag']:
-                print('ligne')
-                if dep[2]==0:
-                    if dep[ind_var]==self.select[0][ind_var]-1:
-                        point=1
-                    elif dep[ind_var]==self.select[1][ind_var]+1:
-                        point=0
-                    else:
-                        self.state="Select"
-                        affiche(self.M)
-                        return
-                    if (dep[ind_fix]==self.select[point][ind_fix] and self.sens in ["ligne","colonne"]) or (dep[1]-dep[0]==self.select[1][1]-self.select[1][0] and self.sens=="diag"):
-                        print(dep,point,self.select)
-                        self.M[dep[0],dep[1]]=self.player
-                        self.M[self.select[point]]=0
+            elif dep[2]==0:
+                if (dep[ind_fix]==self.select[point][ind_fix] and self.sens in ["ligne","colonne"]) or (dep[1]-dep[0]==self.select[1][1]-self.select[1][0] and self.sens=="diag"):
+                    
+                    self.M[dep[0],dep[1]]=self.player
+                    self.M[self.select[1-point]]=0
+                    self.swap()
+                else:
+                    depa=np.array(dep[0:2])
+                    sela=np.array(self.select)
+                    mvt=depa-sela[point]
+                    pts=max(sela[1]-sela[0])
+                    ok=True
+                    SIG={'ligne':(0,1),'colonne':(1,0),'diag':(1,1)}
+                    sig0,sig1=SIG[self.sens]
+                    print(sig0,sig1,pts,sela,point)
+                    for k in range(pts+1):
+                        if self.M[sela[0,0]+mvt[0]+sig0*k, sela[0,1]+mvt[1]+sig1*k]!=0:
+                            ok=False
+                            break
+                    if ok:
+                        for k in range(pts+1):
+                            self.M[sela[0,0]+sig0*k, sela[0,1]+sig1*k]=0
+                            self.M[sela[0,0]+mvt[0]+sig0*k, sela[0,1]+mvt[1]+sig1*k]=self.player
                         self.swap()
+            else:
+                print('sumito')
+                SIG={'ligne':(0,1),'colonne':(1,0),'diag':(1,1)} 
+                sig0,sig1=SIG[self.sens]
+                if point==0:
+                   sig0,sig1=-sig0,-sig1
+                sela=np.array(self.select)
+                pts=max(sela[1]-sela[0])+1
+                nb=0
+                print(sig0,sig1,pts,sela,point)
+                for k in range(1,pts+1):
+                    print(self.M[sela[point][0]+sig0*k, sela[point][0]+sig1*k])
+                    print( sela[point][0]+sig0*k, sela[point][0]+sig1*k)
+                    if self.M[sela[point][0]+sig0*k, sela[point][1]+sig1*k]==self.other:
+                        nb+=1
+                    else:
+                        break
+                else:
+                    print('sumito impossible',pts,nb)
+                    self.state="Select"
+                    affiche(self.M)
+                    return
+                print('nb',nb)
+                if self.M[sela[point,0]+sig0*(nb+1), sela[point,1]+sig1*(nb+1)]==0:
+                    self.M[sela[point][0]+sig0*(nb+1), sela[point][1]+sig1*(nb+1)]=self.other
+                    self.M[sela[point][0]+sig0*(1), sela[point][1]+sig1*(1)]=self.player
+                    self.M[sela[1-point][0], sela[1-point][1]]=0
+                    self.swap()
+                elif self.M[sela[point][0]+sig0*(nb+1), sela[point][1]+sig1*(nb+1)]==3:
+                    self.gain[self.player]+=1
+                    #self.gagner()
+                    self.M[sela[point][0]+sig0*(1), sela[point][1]+sig1*(1)]=self.player
+                    self.M[sela[1-point][0], sela[1-point][1]]=0
+                    self.swap()
+               
+               
+               
         self.state="Select"
         affiche(self.M)
 
@@ -120,7 +185,7 @@ def affiche(M):
             elif M[y,x]==4 or M[y,x]==5:
                 col='green'
             else:
-                col='white'
+                continue
             c=plt.Circle((x+0.5*(5-y),10-y),.45,color=col)
             ax.add_patch(c)
 
